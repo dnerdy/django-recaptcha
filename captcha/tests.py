952cda52af73533bd7a7a658125e972c57a82239
Mark Sandstrom <mark@deliciouslynerdy.com>
2012-04-26 16:10:48 -0700

    Removed fake test

--- captcha/tests.py
+++ captcha/tests.py
@@ -1,8 +1,3 @@
 import unittest
 
-from captcha import fields, forms, models, widgets
-
-
-class TestCase(unittest.TestCase):
-    def test_something(self):
-        raise NotImplementedError('Test not implemented. Bad developer!')
+# TODO Add some tests
